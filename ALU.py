e97db9475ad5da44b04adec70eab62d0ab9442ac
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2022-11-22 01:12:56 -0700

    Fixed unnecessary binary conversion.

--- ALU.py
+++ ALU.py
@@ -1,22 +1,22 @@
 # Class that acts as the Arithmetic Logic Unit of the CPU
 
 class alu:
-    def add(num1, num2):
-        result = int(num1, 2) + int(num2, 2)
+    def add(self, num1, num2):
+        result = num1 + num2
         return result
     
-    def subtract(num1, num2):
-        result = int(num1, 2) - int(num2, 2)
+    def subtract(self, num1, num2):
+        result = num1 - num2
         return result
 
-    def multiply(num1, num2):
-        result = int(num1, 2) * int(num2, 2)
+    def multiply(self, num1, num2):
+        result = num1 * num2
         return result
     
-    def divide(num1, num2):
-        if int(num2, 2) == 0:
+    def divide(self, num1, num2):
+        if num2 == 0:
             print("Cannot divide by zero")
             result = 0
         else:
-            result = int(num1, 2) // int(num2, 2)
+            result = num1 // num2
         return result
