a009b70c847ccf08cee966cd933da162344b5fc9
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2022-11-22 23:30:19 -0700

    Added new function, defined __repr__, and altered instance variables.

--- Registers.py
+++ Registers.py
@@ -2,16 +2,29 @@
 
 class registers:
     def __init__(self):
-        self.reg = [0] * 32
+        self.reg = [0] + [None]*31
+        self.count = 1
+
+    def __repr__(self) -> str:
+        return str(self.reg)
     
     def store(self, value, index):
         print(f"Value {int(value, 2)} stored in register {int(index, 2)}.")
         self.reg[int(index, 2)] = int(value, 2)
+
+    # Function similar to store() that stores a value at the oldest index.
+    # Will overwrite the value at the oldest index based on a count.
+    def store_oldest(self, value):
+        if None in self.reg:
+            self.reg[self.reg.index(None)] = int(value, 2)
+        else:
+            if self.count > 31:
+                self.count = 1
+            self.reg[self.count] = int(value, 2)
+            self.count += 1
 
     def load(self, index):
         return self.reg[int(index, 2)]
     
     def reset(self, index):
         self.reg[int(index, 2)] = 0
-# Need to update to better portray the function of an actual collection of registers.
-# Introduce register overwriting capabilities.
