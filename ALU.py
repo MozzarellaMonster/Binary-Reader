f194fca1768927e65a37744d0fe01b780eb2bf8f
MozzarellaMonster <mozzarellamonster@users.noreply.github.com>
2022-11-23 12:27:50 -0700

    Added program state statements.

--- ALU.py
+++ ALU.py
@@ -2,18 +2,22 @@
 
 class alu:
     def add(self, num1, num2):
+        print("Adding...")
         result = num1 + num2
         return result
     
     def subtract(self, num1, num2):
+        print("Subtracting...")
         result = num1 - num2
         return result
 
     def multiply(self, num1, num2):
+        print("Multiplying...")
         result = num1 * num2
         return result
     
     def divide(self, num1, num2):
+        print("Dividing...")
         if num2 == 0:
             print("Cannot divide by zero")
             result = 0
